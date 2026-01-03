0884f805c7466f88123a7366648766fc19235e75
mavagoncalves <mavagoncalves@gmail.com>
2026-01-03 17:16:39 +0100

    DOM selection for contact.js

---
+++ JS/contact.js
@@ -0,0 +1,9 @@
+// DOM Selection
+const form = document.getElementById('contactForm');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const subjectInput = document.getElementById('subject');
+const messageInput = document.getElementById('message');
+const charCount = document.getElementById('charCount');
+const successMsg = document.getElementById('successMessage');
+
